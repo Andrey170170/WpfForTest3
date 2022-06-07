73baa04f5763b7028a85f2b0b5643d7f9b234f35
Андрей Копанев <24122004@bk.ru>
2022-06-07 16:26:18 +0300

    some more changes

--- WpfForTest3/MainWindow.xaml.cs
+++ WpfForTest3/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfForTest3
 {
@@ -21,54 +12,26 @@ namespace WpfForTest3
     public partial class MainWindow : Window
     {
         private readonly Random _rand = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            int a = 12;
         }
 
         private void ChangeColor_MouseEnter(object sender, MouseEventArgs e)
         {
-            this.StateString.Content = "Изменение цвета";
+            this.StateString.Content = ((MenuItem) sender).Header.ToString();
         }
-
-        private void CatalogGoods_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "Каталог товаров";
-        }
-
-        private void ServiceCatalog_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "Каталог услуг";
-        }
-
-        private void AboutUs_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "О нас";
-        }
-
-        private void FindUs_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "Как найти нас";
-        }
-
         private void ChangeColor_MouseLeave(object sender, MouseEventArgs e)
         {
             this.StateString.Content = string.Empty;
         }
-
-        private void Styles_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "Стили";
-        }
-
-        private void Links_OnMouseEnter(object sender, MouseEventArgs e)
-        {
-            this.StateString.Content = "Сслыки";
-        }
-
+        
         private void ChangeColor_OnClick(object sender, RoutedEventArgs e)
         {
-            this.DockPanel.Background = new SolidColorBrush(Color.FromRgb((byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255)));
+            this.DockPanel.Background = new SolidColorBrush(
+                Color.FromRgb((byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255), (byte)_rand.Next(0, 255)));
         }
 
         private void CatalogGoods_OnClick(object sender, RoutedEventArgs e)
